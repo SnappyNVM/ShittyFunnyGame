dc499c0df020eb320c62b857bc28f4ae40669f54
SnappyNVM <scherbinintimofey@gmail.com>
2023-01-08 15:45:48 +0500

    player

--- PoopButtHoleTheGame/Assets/Scripts/PlayerMovement.cs
+++ PoopButtHoleTheGame/Assets/Scripts/PlayerMovement.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed;
+    [SerializeField] private float speed = 10;
+    [SerializeField] private Animator player;
     private Rigidbody2D playerRigidbody;
-    private Vector3 change; 
+    private Vector3 change;
+    private bool isFacingRight = true;
 
     void Start()
     {
@@ -15,7 +17,11 @@ public class PlayerMovement : MonoBehaviour
     {
         ChangeSetup();
         if (change != Vector3.zero)
+        {
             MoveCharacter();
+            player.SetFloat("Speed", 1);
+        }
+        else player.SetFloat("Speed", 0);
     }
 
     void ChangeSetup()
@@ -28,5 +34,18 @@ public class PlayerMovement : MonoBehaviour
     void MoveCharacter()
     {
         playerRigidbody.MovePosition(transform.position + change.normalized * speed * Time.fixedDeltaTime);
+        FlipCheck();
+    }
+
+    private void FlipCheck()
+    {
+        if (isFacingRight && change.x > 0) Flip();
+        else if (!isFacingRight && change.x < 0) Flip();;
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        this.transform.Rotate(Vector2.up * 180);
     }
 }
